d3cc819889b46b3fcefe8305e11a62f8d2684593
Alina Romanova <alinakhraban@gmail.com>
2023-01-23 16:00:46 +0200

    added functions

---
+++ contacts.js
@@ -0,0 +1,58 @@
+const fsp = require('fs/promises');
+const { nanoid } = require('nanoid');
+const path = require('path'); 
+
+const contactsPath = path.join(__dirname, './db/contacts.json');
+
+const updateContactsList = async (contacts) => await fsp.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+const getAllContacts = async() => {
+  const contacts = await fsp.readFile(contactsPath)
+  return JSON.parse(contacts)
+  
+  }
+  
+ const getContactById = async (contactId) => {
+  const contacts = await getAllContacts();
+  const contact = contacts.find((contact) => contact.id === contactId)
+  if(!contact) {
+    throw new Error("the contact is not found")
+  }
+  return contact;
+  }
+  
+  const removeContact = async (contactId) => {
+    const contacts = await getAllContacts();
+
+    const index = contacts.findIndex((contacts) => contacts.id === contactId)
+
+    contacts.splice(index, 1);
+    await updateContactsList();
+    return contacts[index];
+  }
+
+  
+
+  const addContact = async ({name, email, phone})  => {
+    const contacts = await getAllContacts();
+   const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+   }
+
+   contacts.push(newContact)
+   await updateContactsList(contacts);
+
+   return newContact;
+
+  }
+
+  
+  module.exports = {
+    getAllContacts, 
+    getContactById,
+    removeContact,
+    addContact,
+  }
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const { getAllContacts, getContactById, addContact } = require("./contacts");
+
+run ()
+
+async function run () {
+    try {
+    // const results = await getContactById('7');
+    // console.log(results) 
+    // const newContact = await addContact({name: "Alina", email:"alina@vestibul.co.uk", phone:"(294) 555-6685" });
+    // console.log(newContact)
+    }
+    catch(err) {
+        console.log(err)
+    }
+   
+}
+
+
+
+
